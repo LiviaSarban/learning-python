654067e896429a4d92cc0e53aa687037dd9bd6e3
Livia-Alexandra Sarban <Livia@TodayICode.com>
2019-03-19 18:54:52 +0100

    Updating formatting and comments

--- codecademy/learn-python/control_flow/sals_shipping.md
+++ codecademy/learn-python/control_flow/sals_shipping.md
@@ -27,6 +27,6 @@ Here are the prices:
 
 ## Premium Ground Shipping
 
-*Flat charge*: $125.00
+**Flat charge**: $125.00
 
 Write a program that asks the user for the weight of their package and then tells them which method of shipping is cheapest and how much it will cost to ship their package using Sal’s Shippers.
--- codecademy/learn-python/functions/physics_class.py
+++ codecademy/learn-python/functions/physics_class.py
@@ -4,44 +4,54 @@ train_distance = 100
 
 bomb_mass = 1
 
-# Convert Fahrenheit temperature to Celsius
-def f_to_c(f_temp):
-  c_temp = (f_temp - 32) * 5/9
-  return c_temp
 
-# Test f_to_c function
-f100_in_celsius = f_to_c(100)
+def fahrenheit_to_celsius(fahrenheit_temp):
+    # Convert Fahrenheit temperature to Celsius
+    celsius_temp = (fahrenheit_temp - 32) * 5 / 9
+    return celsius_temp
+
+
+# Test fahrenheit_to_celsius function
+f100_in_celsius = fahrenheit_to_celsius(100)
 print(f100_in_celsius)
 
-# Convert Celsius temperature to Fahrenheit
-def c_to_f(c_temp):
-  f_temp = c_temp * (9/5) + 32
-  return f_temp
 
-# Test c_to_f function
-c0_in_fahrenheit = c_to_f(0)
+def celsius_to_fahrenheit(celsius_temp):
+    # Convert Celsius temperature to Fahrenheit
+    fahrenheit_temp = celsius_temp * (9 / 5) + 32
+    return fahrenheit_temp
+
+
+# Test celsius_to_fahrenheit function
+c0_in_fahrenheit = celsius_to_fahrenheit(0)
 print(c0_in_fahrenheit)
 
-# Find force = mass * acceleration
+
 def get_force(mass, acceleration):
-  return (mass * acceleration)
+    # Find force = mass * acceleration
+    return mass * acceleration
+
 
 # Test get_force function
 train_force = get_force(train_mass, train_acceleration)
 print("The GE train supplies " + str(train_force) + " Newtons of force.")
 
-# Find energy
-def get_energy(mass, c=3*10**8):
-  return(mass*c**2)
+
+def get_energy(mass, c=3 * 10 ** 8):
+    # Find energy
+    return mass * c ** 2
+
 
 # Test get_energy function
 bomb_energy = get_energy(bomb_mass)
 print("A 1kg bomb supplies " + str(bomb_energy) + " Joules.")
 
-# Find work value
+
 def get_work(mass, acceleration, distance):
-  force = get_force(mass, acceleration)
-  return force * distance
+    # Find work value
+    force = get_force(mass, acceleration)
+    return force * distance
+
 
 # Test get_work function
 train_work = get_work(train_mass, train_acceleration, train_distance)
